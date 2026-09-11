f1b97fab250a1ac234e3d44929be5d620097c640
yuliyuqi <1753423026@qq.com>
2026-09-11 20:17:24 +0800

    Delete executor.hpp

--- executor.hpp
+++
@@ -1,7 +0,0 @@
-#pragma once
-#include "shell.hpp"
-
-std::string check_danger(Command* cmd);
-int run_pipeline(Pipeline* pl);
-int run_cmdlist(CmdList* list);
-std::string path_search(const std::string& cmd);
